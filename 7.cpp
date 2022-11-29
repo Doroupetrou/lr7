8819c3b0e2cfb348e36752288188e46c1d96d646
doroupetrou <petrov.ihor.ukr@gmail.com>
2022-11-29 12:38:08 +0100

    remove "your"

---
+++ 7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double y, x;
+    double sum = 0;
+    double mult = 1; 
+    int n, m;
+    
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter y: ";
+    cin >> y;
+    
+    for(n=1; n<=5; n++){
+        sum = sum + cos(n*x) + 5*pow(x, 2);
+        }
+    for(m=2; m<=5; m++){
+        mult = mult * log(m*y);
+    }
+    mult = cos(mult);
+    cout << "Y = " << sum << endl;
+    cout << "Z = " << mult;
+}
